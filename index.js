94a43e0c888456ee58fe887c747db1c2e11a0658
Kara Isreal <isreal.kara92@gmail.com>
2018-09-18 03:50:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var deliLine = [];
+
+function takeANumber(deliLine, name){
+  deliLine.push(name);
+  return ("Welcome, "+ name +". You are number "+ (deliLine.length)+" in line.");
+}
+
+function nowServing(deliLine){
+  if (deliLine.length === 0){
+    return ("There is nobody waiting to be served!");
+  } else {
+    return ("Currently serving"  + ${deliLine.shift()}.);
+}
