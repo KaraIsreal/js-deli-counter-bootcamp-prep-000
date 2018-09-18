64c039d8ecc2ce131573c383d3fc2600e3eaecf4
Kara Isreal <isreal.kara92@gmail.com>
2018-09-18 06:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function currentLine(array){
   if (array.length === 0){
     return "The line is currently empty.";
   } else {
-    var line = [];
-    for (let i = 0; i < array.length; i++);
-    line.push((i+1) + ". " array[i]);
-  return "The line is currently: " line ;}
+      var line = [];
+      for (let i = 0; i < array.length; i++);
+      line.push((i+1) + ". " array[i]);
+    } return "The line is currently: " + line ;
 }
