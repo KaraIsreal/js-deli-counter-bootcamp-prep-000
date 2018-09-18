2ab2844bcd36f75e959851705f914c538e6b2bd2
Kara Isreal <isreal.kara92@gmail.com>
2018-09-18 05:12:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(deliLine){
     return `Currently serving ${deliLine.shift()}.`;}
 }
 
-function currentLine(deliLine){
+function currentLine(line){
   if (deliLine.length === 0){
     return "The line is currently empty.";
   } else {
