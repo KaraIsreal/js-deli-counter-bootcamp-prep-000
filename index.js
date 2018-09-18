da62773d04dc8f66ffc480af7a6506f678fed4b1
Kara Isreal <isreal.kara92@gmail.com>
2018-09-18 06:08:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function currentLine(array){
   } else {
       var line = [];
       for (let i = 0; i < array.length; i++);
-      line.push((i+1) + ". " array[i]);
+      line.push(" " + (i+1) + ". " + array[i] + "");
     } return "The line is currently: " + line;
 }
